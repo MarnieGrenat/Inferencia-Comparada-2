5ddd267b921983fe8e400363689ac2fb80254fbb
Gabriela <92589087+MarnieGrenat@users.noreply.github.com>
2024-06-05 20:23:14 -0400

    Add files via upload

--- questao-1.R
+++ questao-1.R
@@ -1,37 +1,121 @@
-.Machine
+#******************************************************************************************************
+#                   TDE 2 - Inferência Comparada.
+#
+#     Questão: 1
+#       Autor: Gabriela Dellamora Paim
+#      Versão: 2024/06/05
+#
+#******************************************************************************************************
+# Imports
 library(EnvStats)
-# https://search.r-project.org/CRAN/refmans/extraDistr/html/Triangular.html
-# https://cran.r-project.org/web/packages/EnvStats/EnvStats.pdf
-# Método da Transformação Inversa:
-# a < b | a ≤ c ≤ b.
+library(extraDistr)
+library(stats)
+
+# Parâmetros 
 a <- 0
 b <- 2
-c <- 1
+m <- 1
+n <- 5000
+set.seed(222)
+u <- runif(n)
 
-# Obter u de uma U(0, 1)
-# Repetir 5 mil valores
+# Função para inversão da distribuição triangular. Calcula F^-1(u)
 
-set.seed(2)
-u <- runif(5000)
-
-# Calcular F^-1(u)
-x <- qtri(p=u, min=a, max=b, mode=c)
-
-# Plotar Gráfico
-hist(x, 40, freq = FALSE, main = 'Triangular(0, 1, 2)')
-
-# Verificação extra 
-tri_inv <- function(u, a, c, b) {
-  
-  # Valor da função de distribuição cumulativa no ponto c
-  fdc <- (c - a) / (b - a)  
-  
-  # Aplicar a fórmula inversa para a distribuição triangular
+itriang <- function(u, a = -1, b = 1, m = (a + b)/2)
+{
+  fdc <- (m - a) / (b - a)
   ifelse(u < fdc,
-         a + sqrt(u * (b - a) * (c - a)),
-         b - sqrt((1 - u) * (b - a) * (b - c)))
+         a + sqrt(u * (m - a) * (b - a)),
+         b - sqrt((1 - u) * (b - m) * (b - a)))
 }
 
+#******************************************************************************************************
+# a.Simule a densidade de uma Triangular(0, 1, 2) a partir do Método da Transformação Inversa
+#******************************************************************************************************
 
-x <- tri_inv(u, a, c, b)
-hist(x, 40, freq = FALSE, main = 'Triangular(0, 1, 2)')
+## Execução exercício
+tarefa_a <- itriang(u, a, b, m)
+
+## Plotando gráfico
+hist(x,
+     40,
+     freq = FALSE,
+     col = rgb(1, 0.3, 0),
+     xlim = c(a, b),
+     main = 'Transformação Inversa - Triangular(0, 1, 2)'
+     )
+
+## "Prova real"
+prova_real <- qtri(p=u, min=a, max=b, mode=m)
+
+par(mfrow = c(1, 2))
+hist(tarefa_a,
+     40,
+     freq = FALSE,
+     col = rgb(1, 0.3, 0),
+     xlim = c(a, b),
+     main = 'Transformação Inversa - Triangular(0, 1, 2)'
+     )
+
+hist(prova_real,
+     40,
+     freq = FALSE,
+     col = rgb(0, 0.3, 1),
+     xlim = c(a, b),
+     main = 'Prova Real - Triangular(0, 1, 2)'
+     )
+par(mfrow = c(1, 1))
+
+#******************************************************************************************************
+# b.Compare com os resultados de extraDistr::rtriang do R [...]. Dica: use o teste de KS
+#******************************************************************************************************
+## Execução exercício
+x <- itriang(u, a, b, m)
+y <- rtriang(n, a, b, m)
+
+## Plotando gráfico
+par(mfrow = c(1, 2))
+hist(x, 40,
+     freq = FALSE,
+     col = rgb(1, 0.3, 0), 
+     xlim = c(a, b),
+     main = 'ITRIANG(0, 1, 2)',
+     xlab = 'Valor',
+     ylab = 'Densidade'
+     )
+
+hist(y, 40,
+     freq = FALSE,
+     col = rgb(0, 0.3, 1),
+     xlim = c(a, b),
+     main = 'RTRIANG(0, 1, 2)',
+     xlab = 'Valor', ylab = 'Densidade'
+     )
+# Overlap (para identificar D)
+hist(x, 40,
+     freq = FALSE,
+     col = rgb(1, 0.3, 0, 0.3), 
+     xlim = c(a, b),
+     main = 'ITRIANG(0, 1, 2)',
+     xlab = 'Valor',
+     ylab = 'Densidade',
+     add = TRUE
+)
+par(mfrow = c(1, 1))
+
+## Teste KS
+ks.test(x, y)
+
+
+# Análise do Retorno:
+#
+# [ D <- 0.00406 ]
+# D representa a maior diferença absoluta entre as funções.
+# Sendo um valor pequeno (próximo de zero), indica que as distribuições são muito semelhantes.
+
+# [ p-value <- 0.8044 , alpha <- 0.05 (5%) ]
+# H0 <- x = y   (as duas amostras provêm da mesma distribuição)
+# H1 <- x <> y  (As duas amostras não provêm da mesma distribuição)
+# Ou seja: Não existem evidências para rejeitar H0.
+
+# O resultado do teste de KS indica que provavelmente as duas amostras provêm da mesma distribuição.
